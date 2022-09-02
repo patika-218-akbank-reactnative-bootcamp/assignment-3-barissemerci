08af4c9c55bc7af6645c708f963ff2ee7a16ee26
barissemerci <barissemerci3@gmail.com>
2022-09-03 00:12:39 +0300

    theme.js and ThemeProvider.js added

---
+++ TelegramClone/provider/ThemeProvider.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import { ThemeContext } from '../context/theme';
+import dark from '../constants/theme/dark.js';
+import light from '../constants/theme/light.js';
+
+
+
+const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState(light)
+    const handleToggleTheme = (theme) => {
+        if(theme === 'light'){
+            setTheme(light)
+        }else{
+            setTheme(dark)
+        }
+    }
+
+    return(
+        <ThemeContext.Provider value = {{theme, setTheme,toggleTheme:handleToggleTheme}}>
+{children}
+</ThemeContext.Provider>
+
+ 
+    );
+
+};
+
+export default ThemeProvider;
