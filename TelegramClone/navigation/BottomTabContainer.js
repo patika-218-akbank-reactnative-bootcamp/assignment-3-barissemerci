f731cbaf149213eb907a562e5263097dfc6bfa4c
barissemerci <barissemerci3@gmail.com>
2022-09-03 00:11:59 +0300

    BottomTabContainer.js added

---
+++ TelegramClone/navigation/BottomTabContainer.js
@@ -0,0 +1,45 @@
+import *as React from 'react';
+import { StyleSheet, TextInput, TouchableOpacity, View, Text } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ChatsScreen from './screens/ChatsScreen';
+import ContactsScreen from './screens/ContactsScreen';
+import IconContact from 'react-native-vector-icons/AntDesign';
+import IconChat from 'react-native-vector-icons/Entypo';
+import IconSettings from 'react-native-vector-icons/Feather';
+
+import SettingsContainer from './SettingsContainer';
+
+const Tab=createBottomTabNavigator();
+
+
+export default function BottomTabContainer() {
+    return(
+
+        <Tab.Navigator  screenOptions={{
+          headerShown: false,
+        }}>
+            <Tab.Screen options={{
+          tabBarLabel: 'Contact',
+          tabBarIcon: ({ color, size }) => (
+            <IconContact name="contacts" color={color} size={size} />
+          ),
+        }} name={"ContactsScreen"} component={ContactsScreen}></Tab.Screen>
+            <Tab.Screen options={{
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({ color, size }) => (
+            <IconChat name="chat" color={color} size={size} />
+          ),
+        }} name={"ChatsScreen"} component={ChatsScreen}></Tab.Screen>
+            <Tab.Screen options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <IconSettings name="settings" color={color} size={size} />
+          ),
+        }} name={"SettingsContainer"} component={SettingsContainer}></Tab.Screen>
+
+        </Tab.Navigator>
+        
+    )
+    
+}
